a33c68115f4a4ba21c94a2666cdd46f8b709e3c9
Patrick Williams <patrick@stwcx.xyz>
2026-07-06 22:09:06 -0400

    enums: fix ambiguous error in phosphor-logging
    Various users failed with a strange phosphor-logging compile error
    with the switch to concepts from SFINAE:
    
    ```
    ../subprojects/phosphor-logging/lib/include/phosphor-logging/lg2/conversion.hpp:419:50: error: call of overloaded ‘log_convert(const char*, lg2::details::log_flag<>, sdbusplus::common::xyz::openbmc_project::software::ApplyTime::RequestedApplyTimes&)’ is ambiguous
      419 |                                       log_convert(h.data(), log_flag<>{}, v)));
    ```
    
    This is the result of phosphor-logging having a generic enum conversion
    and an sdbusplus conversion that was not properly disambiguated.
    Sprinkle around some decay_t to help disambiguate.  This helps ensure
    that enum-references are properly matched.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I3d402ae9cd9c97e6e82eca6f1d9e089e79afbd82

--- include/sdbusplus/message/native_types.hpp
+++ include/sdbusplus/message/native_types.hpp
@@ -187,6 +187,22 @@ struct convert_to_string
     static std::string op(T) = delete;
 };
 
+// Concept to determine if convert_from_string exists for a type.
+template <typename T>
+concept has_convert_from_string = requires(
+    const std::string& str) { convert_from_string<std::decay_t<T>>::op(str); };
+
+template <typename T>
+inline constexpr bool has_convert_from_string_v = has_convert_from_string<T>;
+
+// Concept to determine if convert_to_string exists for a type.
+template <typename T>
+concept has_convert_to_string =
+    requires(T t) { convert_to_string<std::decay_t<T>>::op(t); };
+
+template <typename T>
+inline constexpr bool has_convert_to_string_v = has_convert_to_string<T>;
+
 } // namespace details
 
 /** @brief Convert from a string to a native type.
